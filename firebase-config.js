bad19567ab901c991e76508cf7f52f32ef535592
Brandon Dunbar <brandon.dunbar97@gmail.com>
2025-12-01 02:42:07 -0500

    Initial commit
    Signed-off-by: Brandon Dunbar <brandon.dunbar97@gmail.com>

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyChrA9OzkGXyBq-0NXg51mzKHj0lNF_CTs",
+  authDomain: "wordgame-93b3f.firebaseapp.com",
+  databaseURL: "https://wordgame-93b3f-default-rtdb.firebaseio.com",
+  projectId: "wordgame-93b3f",
+  storageBucket: "wordgame-93b3f.firebasestorage.app",
+  messagingSenderId: "1093073104170",
+  appId: "1:1093073104170:web:446d2748623cce8ec3c253"
+});
